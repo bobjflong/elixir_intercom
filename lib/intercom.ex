665a17c5d2e98dd8406c9c9edf0c22e1056d1414
Bob Long <robertjflong@gmail.com>
2016-06-20 15:53:31 +0100

    pipes

--- lib/intercom.ex
+++ lib/intercom.ex
@@ -41,9 +41,7 @@ defmodule Intercom do
 
   def snippet(app_id, secret \\ nil, opts) when is_map(opts) do
     base = "<script>(function(){var w=window;var ic=w.Intercom;if(typeof ic===\"function\"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/#{app_id}';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}};})();"
-    ast = boot(
-      inject_user_hash(secret, Map.merge(opts, %{:app_id => app_id}))
-    )
+    ast = secret |> inject_user_hash(Map.merge(opts, %{:app_id => app_id})) |> boot
     base <> Generator.generate(ast) <> ";</script>"
   end
 end
