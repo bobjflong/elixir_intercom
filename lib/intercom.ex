19e329a7feb2f0ff2388300462a0cb570ed9668c
Bob Long <robertjflong@gmail.com>
2016-06-21 09:29:02 +0100

    Use a keyword list when generating snippet

--- lib/intercom.ex
+++ lib/intercom.ex
@@ -40,9 +40,11 @@ defmodule Intercom do
     end
   end
 
-  def snippet(app_id, secret \\ nil, opts) when is_map(opts) do
+  def snippet(props, opts) when is_map(props) do
+    app_id = opts[:app_id]
+    secret = opts[:secret]
     base = "<script>(function(){var w=window;var ic=w.Intercom;if(typeof ic===\"function\"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/#{app_id}';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}};})();"
-    ast = secret |> inject_user_hash(Map.merge(opts, %{:app_id => app_id})) |> boot
+    ast = secret |> inject_user_hash(Map.merge(props, %{:app_id => app_id})) |> boot
     base <> Generator.generate(ast) <> ";</script>"
   end
 end
--- test/intercom_test.exs
+++ test/intercom_test.exs
@@ -35,7 +35,7 @@ defmodule IntercomTest do
 
   test "full snippet" do
     opts = %{:email => "bob@bob.com"}
-    snippet = Intercom.snippet("foo", opts)
+    snippet = Intercom.snippet(opts, app_id: "foo")
     assert snippet == "<script>(function(){var w=window;var ic=w.Intercom;if(typeof ic===\"function\"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/foo';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}};})();Intercom('boot',{\n        app_id: 'foo',     email: 'bob@bob.com'\n  });</script>"
   end
 end
