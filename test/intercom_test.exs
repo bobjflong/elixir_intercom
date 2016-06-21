de61afcd7c0d882f0d22c3d31d9c5bef1832a7fd
Bob Long <robertjflong@gmail.com>
2016-06-21 09:35:03 +0100

    Support nonce attribute

--- test/intercom_test.exs
+++ test/intercom_test.exs
@@ -38,4 +38,10 @@ defmodule IntercomTest do
     snippet = Intercom.snippet(opts, app_id: "foo")
     assert snippet == "<script>(function(){var w=window;var ic=w.Intercom;if(typeof ic===\"function\"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/foo';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}};})();Intercom('boot',{\n        app_id: 'foo',     email: 'bob@bob.com'\n  });</script>"
   end
+
+  test "full snippet with nonce" do
+    opts = %{:email => "bob@bob.com"}
+    snippet = Intercom.snippet(opts, app_id: "foo", csp_nonce: "bar")
+    assert snippet == "<script nonce=\"bar\">(function(){var w=window;var ic=w.Intercom;if(typeof ic===\"function\"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/foo';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}};})();Intercom('boot',{\n        app_id: 'foo',     email: 'bob@bob.com'\n  });</script>"
+  end
 end
